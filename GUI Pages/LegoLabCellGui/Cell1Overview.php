f2240d65ed2b7435d87c3ae759d1d46b0596817b
Zach Ogle <zach_ogle@bellsouth.net>
2013-04-17 15:03:22 -0500

    Final web pages

---
+++ GUI Pages/LegoLabCellGui/Cell1Overview.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" http-equiv="refresh" content="5">
+<title>Cell 1 Overview</title>
+<link href="../jquery.mobile-1.3.0/jquery.mobile-1.3.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="../jquery.mobile-1.3.0/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="../jquery.mobile-1.3.0/jquery.mobile-1.3.0.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="cellgui.css"/>
+</head>
+
+<body>
+<?php
+function display()
+{
+	//Database Information
+	$db_host = "localhost";
+	$db_username = "LegoLab";
+	$db_pass = "";
+	$db_name = "legolab";
+	
+	//Connect to the database
+	$con = mysqli_connect($db_host, $db_username, $db_pass, $db_name);
+	$stations = mysqli_query($con, "SELECT * FROM station WHERE cell = 1 ORDER BY station");
+	
+	echo '
+	<table class="stationOverview">
+	<tr>';
+	
+	date_default_timezone_set('America/Chicago');		  
+	$count = 0;
+	
+	while ($stationRow = mysqli_fetch_array($stations))
+	{
+		$stationId = $stationRow['station_id'];
+		$latestInfo = mysqli_query($con, "SELECT * FROM latest_info WHERE station = $stationId");
+		$infoRow = mysqli_fetch_array($latestInfo);
+		$currentTime = date("His");
+		$timeDifference = $currentTime - $infoRow['time_since_defect'];
+		$timeDifference = gmdate("H:i:s", $timeDifference);
+		if ($stationRow['station'] != 0)
+		{
+			echo '
+			<td>
+			<a href="Cell' . $stationRow['cell'] . 'Station' . $stationRow['station'] . '.php" rel="external" data-role="button">
+			<table class="station">
+			<tr>
+			<td width=60%><h2 align="left">Station ' . $stationRow['station'] . ':</h2>
+			<p align="left">Average Process Time: ' . $infoRow['average_process_time'] . '</p>
+			<p align="left">Average Idle Time: ' . $infoRow['average_idle_time'] . '</p>
+			<p align="left">Cycle Time: ' . $infoRow['takt_time'] . '</p>
+			<p align="left">Daily Defects: ' . $infoRow['daily_defect'] . '</p>
+			<p align="left">Time Since Defect: ' . $timeDifference . '</p>
+			<p>&nbsp;</p>
+			</td>';
+		}
+		else
+		{
+			echo '
+			<td>
+			<a href="Cell' . $stationRow['cell'] . 'Overall.php" rel="external" data-role="button">
+			<table class="station">
+			<tr>
+			<td width=60%><h2 align="left">Overall:</h2>
+			<p align="left">Average Process Time: ' . $infoRow['average_process_time'] . '</p>
+			<p align="left">Average Idle Time: ' . $infoRow['average_idle_time'] . '</p>
+			<p align="left">Lead Time: ' . $infoRow['takt_time'] . '</p>
+			<p align="left">Daily Defects: ' . $infoRow['daily_defect'] . '</p>
+			<p align="left">Time Since Defect: ' . $timeDifference . '</p>
+			<p align="left">Bottleneck Station: ' . $infoRow['bottleneck'] . '</p>
+			</td>';
+		}
+		$statusNumber = $infoRow['status'];
+		$status = mysqli_query($con, "SELECT status FROM status WHERE status_id = $statusNumber");
+		$statusRow = mysqli_fetch_array($status);
+		echo '
+		<td width="40%" class="' . $statusRow['status'] . '">&nbsp;</td>
+		</tr>
+		</table>
+		</a>
+		</td>';
+		if ($count % 2 != 0)
+		{
+			echo '
+			</tr>
+			<tr>';
+		}
+		$count++;
+	}
+	
+	echo '
+	</tr>
+	</table>';
+}
+?>
+
+<div data-role="page">
+	<div data-role="header">
+    	<a href="CellOverview.php" data-rel="back" data-icon="back">Back</a>
+		<h1>Tiger Automotive Lab: Cell 1</h1>
+	</div>
+	<div data-role="content">
+    	<?php display(); ?>
+  </div>
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div>
+</div>
+</body>
+</html>
---
+++ GUI Pages/LegoLabCellGui/CellOverview.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" http-equiv="refresh" content="5">
+<title>Cell Overview</title>
+<link href="./jquery.mobile-1.3.0/jquery.mobile-1.3.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="./jquery.mobile-1.3.0/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="./jquery.mobile-1.3.0/jquery.mobile-1.3.0.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="cellgui.css"/>
+</head>
+<?php
+function display()
+{
+	//Database Information
+	$db_host = "localhost";
+	$db_username = "LegoLab";
+	$db_pass = "";
+	$db_name = "legolab";
+	
+	//Connect to the database
+	$con = mysqli_connect($db_host, $db_username, $db_pass, $db_name);
+	$cells = mysqli_query($con, "SELECT DISTINCT cell FROM station ORDER BY cell");
+	$stations = mysqli_query($con, "SELECT * FROM station ORDER BY cell, station");
+	
+	while ($cellRow = mysqli_fetch_array($cells))
+	{
+		echo '
+		<a href="Cell' . $cellRow['cell'] . 'Overview.php" rel="external" data-role="button">
+		<h3 align="left">Cell ' . $cellRow['cell'] . ':</h3>
+		<table class="cellOverview">
+		<tr>';
+		while (($stationRow = mysqli_fetch_array($stations)) && $stationRow['cell'] == $cellRow['cell'])
+		{
+			if ($stationRow['station'] != 0)
+			{
+				$stationId = $stationRow['station_id'];
+				$status = mysqli_query($con, "SELECT status FROM latest_info WHERE station = $stationId");
+				$statusRow = mysqli_fetch_array($status);
+				$statusNumber = $statusRow['status'];
+				$status = mysqli_query($con, "SELECT status FROM status WHERE status_id = $statusNumber");
+				$statusRow = mysqli_fetch_array($status);
+				echo '
+				<td class="' . $statusRow['status'] . '">&nbsp;</td>';
+			}
+		}
+		echo '
+		</tr>
+		</table>
+		</a>';
+	}
+	echo '
+	<h3 align="left">Overall:</h3>';
+	$stations = mysqli_query($con, "SELECT * FROM station ORDER BY cell, station");
+	while ($stationRow = mysqli_fetch_array($stations))
+	{
+		$cell = $stationRow['cell'];
+		$station = $stationRow['station'];
+		$stationId = $stationRow['station_id'];
+		$error = mysqli_query($con, "SELECT error_type FROM latest_info WHERE station = $stationId");
+		$errorRow = mysqli_fetch_array($error);
+		switch($errorRow['error_type'])
+		{
+			case 1:
+				break;
+			case 2:
+				if ($station != 0)
+				{
+					echo '
+					<p>Cell ' . $cell . ' Station ' . $station . ': Process time value outside of control limits.</p>';
+				}
+				break;
+			case 3:
+				if ($station != 0)
+				{
+					echo '
+					<p>Cell ' . $cell . ' Station ' . $station . ': Idle time value outside of control limits.</p>';
+				}
+				break;
+			case 4:
+				if ($station != 0)
+				{
+					$defects = mysqli_query($con, "SELECT location FROM defect WHERE sensor=$stationId ORDER BY timestamp DESC, location ASC");
+					while ($defectRow = mysqli_fetch_array($defects))
+					{
+						$location = $defectRow['location'];
+						echo '
+						<p>Cell ' . $cell . ' Station ' . $station . ' Shape ' . $location .': Incorrect color.</p>';
+					}
+				}
+				break;				
+		}
+	}
+}
+?>
+<body>
+<div data-role="page">
+	<div data-role="header">
+		<h1>Tiger Automotive Lab Status</h1>
+	</div>
+	<div data-role="content">
+    	<?php display(); ?>
+    </div>
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div>
+</div>
+</body>
+</html>
